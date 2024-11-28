0ee2f51c3993735ce795014328c9f2f44f810cbb
WonYunSun <ereunosun113@gmail.com>
2024-11-28 20:36:29 +0900

    zustand  삭제

--- src/store/useUserStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useUserStore = create((set) => ({
-  nickname: "",
-  setNickname: (newNickname) => set({ nickname: newNickname }), // 닉네임을 업데이트하는 함수
-}));
-
-export default useUserStore;
